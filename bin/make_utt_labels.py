ba94eb20e0334d1c603b383f9f11ab232e8faa76
Beanocean <beanocean@outlook.com>
2014-08-08 15:31:05 +0800

    make labels for reshaped feature

--- bin/make_utt_labels.py
+++ bin/make_utt_labels.py
@@ -1,23 +1,39 @@
-#!/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ===================================================================
+#     FileName: make_utt_labels.py
+#       Author: Beanocean
+#        Email: beanocean@outlook.com
+#   CreateTime: 2014-08-08 15:00
+# ===================================================================
 
-import os
-import sys
+def _get_label_list(feat_file):
 
-feats = open(sys.argv[1])
-label_file = open(sys.argv[2], 'w')
-genre_list = {'baroque':0, 'blues':1, 'classical':2, 'country':3, \
+    feats = open(feat_file)
+    label_list = ''
+    genre_list = {'baroque':0, 'blues':1, 'classical':2, 'country':3, \
         'electronica':4, 'hip':5, 'jazz':6, 'metal':7, 'rock':8, 'romantic':9}
-label = 0
 
-for line in feats:
-    if '[' in str(line):
+    for line in feats:
         song_id = line.split(' ')[0]
         genre = song_id.split('-')[0]
-        label_file.write(song_id + ' ')
-    else:
-        label_file.write(str(genre_list[genre]) + ' ')
-        if ']' in str(line):
-            label_file.write('\n')
-feats.close()
-label_file.close()
+        label_list = label_list + str(genre_list[genre]) + ' '
 
+    feats.close()
+    return label_list
+
+def write_file(old_scp, new_scp, lab_file):
+
+    label_list = _get_label_list(old_scp)
+    nscp_fid = open(new_scp)  # new scp file id
+    lab_fid = open(lab_file, 'w')  # label file id
+
+    for line in nscp_fid:
+        utt_id = line.split(' ')[0]
+        lab_fid.write(utt_id + ' ' + label_list + '\n')
+    nscp_fid.close()
+    lab_fid.close()
+
+if __name__ == '__main__':
+    import sys
+    write_file(sys.argv[1], sys.argv[2], sys.argv[3])
