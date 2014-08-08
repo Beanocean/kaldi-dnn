fae70612edfd13d4287babfecb6ea6194bea7ea7
Beanocean <beanocean@outlook.com>
2014-08-08 15:34:58 +0800

    edit main scripts to add former functions

--- myrun.sh
+++ myrun.sh
@@ -6,51 +6,91 @@
 #   CreateTime: 2014-08-01 15:49
 # ===================================================================
 
-# environment configuration
-. ./cmd.sh
-[ -f path.sh ] && . ./path.sh
-set -e
+# Default configuration.
+nj=10
 stage=0
 
-# how to use this shell scripts
+# Environment configuration
+. ./cmd.sh
+[ -f path.sh ] && . ./path.sh
+. utils/parse_options.sh || exit 1;
+set -e
+
+
+# How to use this shell scripts
 if [ ! $# -eq  2 ]; then
-  echo "USAGE: $0 <prefix_name> <feat-type>"
-  echo "feat-types: mfcc, spectralband, spectrogram"
+  echo "usage: $0 [options] <prefix_name> <feat-type>"
+  echo "opthions: "
+  echo "  --stage <stage>     # resume the script from certain stage."
+  echo "feat-types: "
+  echo "  mfcc, spectralband, spectrogram"
   exit 1
 fi
 
 prefix=../results/$1
-
-# check feats type
 feats=$2
+
+# Check feature type
 if [ "$feats" != "mfcc" ] && [ "$feats" != "spectralband" ] \
   && [ "$feats" != "spectrogram" ]; then
   echo "feats-type must be one of: mfcc, spectralband, spectrogram" && exit 1
 fi
 
-# extract feature
+# Extract feature
 if [ $stage -le 0 ]; then
-  # make directories of current experiments
-  [ -e $prefix ] && echo "remove old data in $prefix" && rm -r $prefix
+  # make directories for current experiments
+  # [ -e $prefix ] && echo "remove old data in $prefix" && rm -r $prefix
   mkdir -p $prefix
-  cp -r data $prefix/data
+  cp -r data $prefix/
+
+  # shuffle wav.scp
+  mv $prefix/data/train/wav.scp $prefix/data/train/wav.scp.old
+  shuf $prefix/data/train/wav.scp.old -o $prefix/data/train/wav.scp
+
+  # prepare utt2spk & spk2utt, extract train & test data
+  find $prefix/data/{train,test}/wav.scp > /dev/null
+  [ $? -eq 1 ] && exit 1;
 
   for x in train test; do 
-    steps/make_${feats}.sh --cmd "$train_cmd" --nj 10 $prefix/data/$x \
+    # bin/make_utt_spk.sh $prefix/data/$x/wav.scp  # make utt2spk & spk2utt
+    steps/make_${feats}.sh --cmd "$train_cmd" --nj $nj $prefix/data/$x \
       $prefix/exp/$feats/$x $prefix/$feats
   done
 fi
 
-stage=2
+# Reshape feature
 if [ $stage -le 1 ]; then
-  # reshape all feature and make label
-  dir=$prefix/data/train
-  # bin/cat_into_one.sh $dir/feats.scp $dir/allfeat.txt
-  python bin/make_new_feats.py $dir/allfeat.txt $dir/newfeats.txt
-fi
+  # reshape all feature
+  dir=$prefix/$feats; cmd=run.pl; logdir=$prefix/exp/reshape_feature
+  echo "cmd: "$cmd
+  echo "logdir: "$logdir
+  $cmd JOB=1:$nj $logdir/reshape_feature_train.JOB.log \
+    bin/reshape_feats.sh $dir/raw_${feats}_train.JOB.ark \
+    $dir/new_${feats}_train.JOB.ark utt-JOB
 
-stage=3
+  $cmd JOB=1:$nj $logdir/make_label_train.JOB.log \
+    bin/make_utt_labels.py $dir/raw_${feats}_train.JOB.scp \
+    $dir/new_${feats}_train.JOB.scp $dir/new_${feats}_train.JOB.labels
+
+  mv $prefix/data/train/feats.scp $prefix/data/train/feats.scp.old
+  rm $prefix/data/train/labes.pdf -f
+  for i in $(seq $nj); do
+    cat $dir/new_${feats}_train.${x}.scp >> $prefix/data/train/feats.scp
+    cat $dir/new_${feats}_train.${x}.labels >> $prefix/data/train/labels.pdf
+  done
+fi
+stage=4
+# Train dnn
 if [ $stage -le 2 ]; then
-  # train dnn
+  # pretrain & train
   local/run_dnn.sh $prefix
 fi
+
+# Generate learned-feature
+if [ $stage -le 3 ]; then
+  dir=$prefix/learned_feature
+  mkdir $dir
+  bin/test_dnn.sh $dir
+fi
+
+echo "finshed successfully!"
